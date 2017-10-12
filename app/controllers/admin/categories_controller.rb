9f2954c1dc1ebb84f90ce944cba10bdf0ac5d2c8
Scotty <scott.hagan@aol.com>
2017-10-12 21:58:04 +0000

    added basic auth for admin controllers

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
-
+  http_basic_authenticate_with name: "Jungle", password: "Book"
+  
   def index
     @categories = Category.order(id: :desc).all  
   end
