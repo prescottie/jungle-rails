21d1df2170cfe997e55b7b99864f613c1a701ffe
Scotty <scott.hagan@aol.com>
2017-10-13 02:09:20 +0000

    working on the review form submission and saving to db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,24 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm Here!"
+    @review = Review.new(review_params)
+
+    if @product.save!
+      redirect_to "product_path", notice: 'Review created!'
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def product_params
+    params.require(:review).permit(
+      :product_id,
+      :user_id,
+      :description,
+      :rating
+    )
   end
 end
