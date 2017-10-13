a78194cf8dc7af78b5621f5e5130cc68b3dbf672
Scotty <scott.hagan@aol.com>
2017-10-13 05:07:12 +0000

    adding reviews now saves to db and displays on page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,8 +3,10 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
 
-    if @product.save!
-      redirect_to "product_path", notice: 'Review created!'
+    @review.user = current_user
+
+    if @review.save!
+      redirect_to :back, notice: 'Review created!'
     else
       render :new
     end
@@ -13,7 +15,7 @@ class ReviewsController < ApplicationController
 
 
   private
-  def product_params
+  def review_params
     params.require(:review).permit(
       :product_id,
       :user_id,
