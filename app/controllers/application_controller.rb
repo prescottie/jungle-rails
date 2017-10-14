8740e79b3266c2e4590520ea7f9ec607f415da3c
Scotty <scott.hagan@aol.com>
2017-10-14 00:52:25 +0000

    added reviews with ratings to products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,17 +34,6 @@ class ApplicationController < ActionController::Base
 
   def authorize
     redirect_to '/login' unless current_user
-  end
-
-  # def http_basic_auth
-  #   if ENV['HTTP_AUTH'] =~ %r{(.+)\:(.+)}
-  #     unless authenticate_with_http_basic { |user, password|  user == $1 && password == $2 }
-  #       request_http_basic_authentication
-  #     end
-  #   end
-  # end
-
-  
-  
+  end  
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,31 @@
 class ReviewsController < ApplicationController
 
+  before_filter :authorize
+
   def create
     @review = Review.new(review_params)
-
+    @product = Product.find(params[:product_id])
     @review.user = current_user
+    @review.product = @product
 
     if @review.save!
       redirect_to :back, notice: 'Review created!'
     else
-      render :new
+      render template: 'products/show'
     end
+  end
+  
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy!
+    redirect_to :back, notice: 'Review deleted!'
   end
 
 
 
   private
   def review_params
-    params.require(:review).permit(
-      :product_id,
-      :user_id,
-      :description,
-      :rating
-    )
+    params.require(:review).permit( :description, :rating )
   end
+
 end
